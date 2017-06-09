058fdd4e5fededf16d4daad2f6a9829bdd42d145
qiufujian1216 <1170644070@qq.com>
2017-06-09 14:16:45 +0800

    6.9

--- duodian/src/router/index.js
+++ duodian/src/router/index.js
@@ -17,24 +17,26 @@ import duodianmarket from '../pages/home/duodianmarket'
 import globalselect from '../pages/home/globalselect'
 import domestic_fruit from '../pages/classify/domestic_fruit'
 import import_fruit from '../pages/classify/import_fruit'
+import search from '../pages/home/search'//首页搜索页
 
 // 5.定义路由规则
 const routes = [
-	{path:'',component:home,children:[{
+	{path:'',component:home,children:[{//默认页面
 		path:'',component:duodianmarket}]},
-	{path:'/home',component:home,children:[
+	{path:'/home',component:home,children:[//首页
 		{path:'',component:duodianmarket},
 		{path:'duodianmarket',component:duodianmarket},
 		{path:'globalselect',component:globalselect},
 	]},
-	{path:'/classify',component:classify,children:[
+	{path:'/classify',component:classify,children:[//分类
 		{path:'',component:domestic_fruit},
 		{path:'domestic_fruit',component:domestic_fruit},
 		{path:'import_fruit',component:import_fruit},
 	]},
-	{path:'/member',component:member},
-	{path:'/cart',component:cart},
-	{path:'/mine',component:mine},
+	{path:'/member',component:member},//会员
+	{path:'/cart',component:cart},//购物车
+	{path:'/mine',component:mine},//我的
+	{path:'/search',component:search}//首页搜索
 ]
 
 // 6.创建路由对象
